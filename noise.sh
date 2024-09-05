37ae86c7031f986230db610b799442793d119659
dehuang <dan.e.huang@gmail.com>
2024-09-05 07:28:10 +0000

    noise

---
+++ noise.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+DEBUG=false
+
+DATA_DIR=data/uci_datasets/uci_datasets
+DATASETS=(pol elevators bike kin40k protein keggdirected slice keggundirected 3droad song buzz houseelectric)
+
+GROUP=noise
+EPOCHS=50
+NUM_INDUCING=512
+DEVICE="cuda:1"
+SEEDS=(6535)
+
+if $DEBUG; then
+    EPOCHS=1
+fi
+
+for dataset in "${DATASETS[@]}"
+do
+    for seed in "${SEEDS[@]}"
+    do
+        python train.py \
+            --model.name soft-gp \
+            --model.num_inducing $NUM_INDUCING \
+            --model.device $DEVICE \
+            --model.use_qr \
+            --model.use_scale \
+            --model.learn_noise \
+            --data_dir $DATA_DIR \
+            --dataset.name $dataset \
+            --training.seed $seed \
+            --training.epochs $EPOCHS \
+            --training.learning_rate 0.01 \
+            --dataset.train_frac 0.9 \
+            --dataset.val_frac 0 \
+            --wandb.group $GROUP \
+            --wandb.watch
+
+        python train.py \
+            --model.name soft-gp \
+            --model.num_inducing $NUM_INDUCING \
+            --model.device $DEVICE \
+            --model.use_qr \
+            --model.use_scale \
+            --data_dir $DATA_DIR \
+            --dataset.name $dataset \
+            --training.seed $seed \
+            --training.epochs $EPOCHS \
+            --training.learning_rate 0.01 \
+            --dataset.train_frac 0.9 \
+            --dataset.val_frac 0 \
+            --wandb.group $GROUP \
+            --wandb.watch
+    done
+done
