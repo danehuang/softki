ae36daaed2f0ce57c6bee2e7da3d90c06be71ec0
dehuang <dan.e.huang@gmail.com>
2024-09-06 02:57:19 +0000

    inducing

---
+++ inducing.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+DEBUG=false
+
+DATA_DIR=data/uci_datasets/uci_datasets
+DATASETS=(pol elevators bike kin40k protein keggdirected slice keggundirected 3droad song buzz houseelectric)
+# DATASETS=(kin40k)
+
+GROUP=inducing
+EPOCHS=50
+NUM_INDUCING=(256 512 1024 2048 4096)
+BATCH_SIZE=(1024)
+DEVICE="cuda:1"
+SEEDS=(6535 8830 92357)
+
+if $DEBUG; then
+    EPOCHS=1
+fi
+
+for dataset in "${DATASETS[@]}"
+do
+    for seed in "${SEEDS[@]}"
+    do
+        for num_inducing in "${NUM_INDUCING[@]}"
+        do
+            for batch_size in "${BATCH_SIZE[@]}"
+            do
+                python train.py \
+                    --model.name soft-gp \
+                    --model.num_inducing $num_inducing \
+                    --model.device $DEVICE \
+                    --model.use_qr \
+                    --model.use_scale \
+                    --data_dir $DATA_DIR \
+                    --dataset.name $dataset \
+                    --training.seed $seed \
+                    --training.epochs $EPOCHS \
+                    --training.batch_size $batch_size \
+                    --training.learning_rate 0.01 \
+                    --dataset.train_frac 0.9 \
+                    --dataset.val_frac 0 \
+                    --wandb.group $GROUP \
+                    --wandb.watch
+            done
+        done
+    done
+done
