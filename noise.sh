17684423723d903362f09aba353d15788ab19456
dehuang <dan.e.huang@gmail.com>
2024-09-08 22:29:01 +0000

    noise

--- noise.sh
+++ noise.sh
@@ -8,8 +8,8 @@ DATASETS=(pol elevators bike kin40k protein keggdirected slice keggundirected 3d
 GROUP=noise
 EPOCHS=50
 NUM_INDUCING=512
-DEVICE="cuda:1"
-SEEDS=(6535)
+DEVICE="cuda:0"
+SEEDS=(8830 92357)
 
 if $DEBUG; then
     EPOCHS=1
@@ -36,20 +36,20 @@ do
             --wandb.group $GROUP \
             --wandb.watch
 
-        python train.py \
-            --model.name soft-gp \
-            --model.num_inducing $NUM_INDUCING \
-            --model.device $DEVICE \
-            --model.use_qr \
-            --model.use_scale \
-            --data_dir $DATA_DIR \
-            --dataset.name $dataset \
-            --training.seed $seed \
-            --training.epochs $EPOCHS \
-            --training.learning_rate 0.01 \
-            --dataset.train_frac 0.9 \
-            --dataset.val_frac 0 \
-            --wandb.group $GROUP \
-            --wandb.watch
+        # python train.py \
+        #     --model.name soft-gp \
+        #     --model.num_inducing $NUM_INDUCING \
+        #     --model.device $DEVICE \
+        #     --model.use_qr \
+        #     --model.use_scale \
+        #     --data_dir $DATA_DIR \
+        #     --dataset.name $dataset \
+        #     --training.seed $seed \
+        #     --training.epochs $EPOCHS \
+        #     --training.learning_rate 0.01 \
+        #     --dataset.train_frac 0.9 \
+        #     --dataset.val_frac 0 \
+        #     --wandb.group $GROUP \
+        #     --wandb.watch
     done
 done
