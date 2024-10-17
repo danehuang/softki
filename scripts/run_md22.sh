806f2ce8d21abe3c015c761d6660f1fe130381c6
dehuang <dan.e.huang@gmail.com>
2024-10-17 06:32:43 +0000

    rename

---
+++ scripts/run_md22.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+DEBUG=false
+
+DATA_DIR=data/
+DATASETS=(Ac-Ala3-NHMe AT-AT-CG-CG AT-AT stachyose DHA buckyball-catcher double-walled-nanotube)
+
+PROJECT=softki2
+GROUP=md22
+EPOCHS=50
+DEVICE="cuda:0"
+SEEDS=(6535 8830 92357)
+
+if $DEBUG; then
+    EPOCHS=1
+fi
+
+pushd ..
+    for dataset in "${DATASETS[@]}"
+    do
+        for seed in "${SEEDS[@]}"
+        do
+            python train.py \
+                --model.name soft-gp \
+                --model.num_inducing 512 \
+                --model.device $DEVICE \
+                --model.use_qr \
+                --model.use_scale \
+                --model.T 1 \
+                --data_dir $DATA_DIR \
+                --dataset.name $dataset \
+                --training.seed $seed \
+                --training.epochs $EPOCHS \
+                --training.learning_rate 0.01 \
+                --dataset.train_frac 0.9 \
+                --dataset.val_frac 0 \
+                --wandb.project $PROJECT \
+                --wandb.group $GROUP \
+                --wandb.watch
+
+            python train.py \
+                --model.name svi-gp \
+                --model.num_inducing 1024 \
+                --model.device $DEVICE \
+                --model.learn_noise \
+                --model.use_scale \
+                --data_dir $DATA_DIR \
+                --dataset.name $dataset \
+                --training.seed $seed \
+                --training.epochs $EPOCHS \
+                --training.learning_rate 0.01 \
+                --dataset.train_frac 0.9 \
+                --dataset.val_frac 0 \
+                --wandb.project $PROJECT \
+                --wandb.group $GROUP \
+                --wandb.watch
+
+            python train.py \
+                --model.name sv-gp \
+                --model.num_inducing 512 \
+                --model.device $DEVICE \
+                --model.learn_noise \
+                --model.use_scale \
+                --data_dir $DATA_DIR \
+                --dataset.name $dataset \
+                --training.seed $seed \
+                --training.epochs $EPOCHS \
+                --training.learning_rate .1 \
+                --dataset.train_frac 0.9 \
+                --dataset.val_frac 0 \
+                --wandb.project $PROJECT \
+                --wandb.group $GROUP \
+                --wandb.watch
+        done
+    done
+popd
---
+++ scripts/run_noise.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+DEBUG=false
+
+DATA_DIR=data/uci_datasets/uci_datasets
+DATASETS=(pol elevators bike kin40k protein keggdirected slice keggundirected 3droad song buzz houseelectric)
+
+PROJECT=softki2
+GROUP=noise
+EPOCHS=50
+NUM_INDUCING=512
+DEVICE="cuda:0"
+SEEDS=(6535 8830 92357)
+
+if $DEBUG; then
+    EPOCHS=1
+fi
+
+pushd ..
+    for dataset in "${DATASETS[@]}"
+    do
+        for seed in "${SEEDS[@]}"
+        do
+            python train.py \
+                --model.name soft-gp \
+                --model.num_inducing $NUM_INDUCING \
+                --model.device $DEVICE \
+                --model.use_qr \
+                --model.use_scale \
+                --model.learn_noise \
+                --model.T 1 \
+                --data_dir $DATA_DIR \
+                --dataset.name $dataset \
+                --training.seed $seed \
+                --training.epochs $EPOCHS \
+                --training.learning_rate 0.01 \
+                --dataset.train_frac 0.9 \
+                --dataset.val_frac 0 \
+                --wandb.group $GROUP \
+                --wandb.watch
+        done
+    done
+popd
