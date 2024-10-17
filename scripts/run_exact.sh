74dc10f4eab4fa347184b1523da7ea8423f29d1b
dehuang <dan.e.huang@gmail.com>
2024-10-17 06:33:37 +0000

    move

---
+++ scripts/run_exact.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DEBUG=false
+
+DATA_DIR=data/uci_datasets/uci_datasets
+# DATASETS=(pol elevators bike kin40k protein keggdirected slice keggundirected 3droad song buzz houseelectric)
+DATASETS=(pol elevators)
+
+PROJECT=softki2
+GROUP=benchmark
+EPOCHS=40
+DEVICE="cuda:0"
+SEEDS=(6535 8830 92357)
+
+if $DEBUG; then
+    EPOCHS=1
+fi
+
+pushd ..
+    for dataset in "${DATASETS[@]}"
+    do
+        for seed in "${SEEDS[@]}"
+        do
+            python train.py \
+                --model.name exact \
+                --model.device $DEVICE \
+                --model.use_scale \
+                --data_dir $DATA_DIR \
+                --dataset.name $dataset \
+                --training.seed $seed \
+                --training.epochs $EPOCHS \
+                --training.learning_rate 0.1 \
+                --dataset.train_frac 0.9 \
+                --dataset.val_frac 0 \
+                --wandb.project $PROJECT \
+                --wandb.group $GROUP \
+                --wandb.watch
+
+        done
+    done
+popd
---
+++ scripts/run_inducing.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+DEBUG=false
+
+DATA_DIR=data/uci_datasets/uci_datasets
+DATASETS=(pol elevators bike kin40k protein keggdirected slice keggundirected 3droad song buzz houseelectric)
+
+PROJECT=softki2
+GROUP=inducing
+EPOCHS=50
+NUM_INDUCING=(64 128 256 512 1024 1536 2048 4096)
+BATCH_SIZE=(1024)
+DEVICE="cuda:0"
+SEEDS=(6535 8830 92357)
+
+if $DEBUG; then
+    EPOCHS=1
+fi
+
+pushd ..
+    for dataset in "${DATASETS[@]}"
+    do  
+        for num_inducing in "${NUM_INDUCING[@]}"
+        do
+            for seed in "${SEEDS[@]}"
+            do
+                for batch_size in "${BATCH_SIZE[@]}"
+                do
+                    python train.py \
+                        --model.name soft-gp \
+                        --model.num_inducing $num_inducing \
+                        --model.device $DEVICE \
+                        --model.use_qr \
+                        --model.use_scale \
+                        --data_dir $DATA_DIR \
+                        --dataset.name $dataset \
+                        --training.seed $seed \
+                        --training.epochs $EPOCHS \
+                        --training.batch_size $batch_size \
+                        --training.learning_rate 0.01 \
+                        --dataset.train_frac 0.9 \
+                        --dataset.val_frac 0 \
+                        --wandb.project $PROJECT \
+                        --wandb.group $GROUP \
+                        --wandb.watch
+                done
+            done
+        done
+    done
+popd
