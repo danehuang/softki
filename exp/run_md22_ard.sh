85642afaca27f05d537ae7363f8539989dae2e6e
Daniel Huang <dan.e.huang@gmail.com>
2024-12-02 07:14:17 +0000

    adding md22 ard

--- exp/run_md22_ard.sh
+++ exp/run_md22_ard.sh
@@ -1,31 +1,50 @@
 #!/bin/bash
 
-DEBUG=true
+DEBUG=false
 
-DATA_DIR=data/uci_datasets/uci_datasets
-DATASETS=(pol elevators bike kin40k protein keggdirected slice keggundirected 3droad song buzz houseelectric)
+DATA_DIR=data/
+DATASETS=(Ac-Ala3-NHMe AT-AT-CG-CG AT-AT stachyose DHA buckyball-catcher double-walled-nanotube)
 
 PROJECT=softki2
-GROUP=nonoise
+GROUP=md22-ard
 EPOCHS=50
-DEVICE="cuda:0"
-NUM_WORKERS=0
+DEVICE="cuda:1"
 SEEDS=(6535 8830 92357)
 
 if $DEBUG; then
     EPOCHS=1
-    SEEDS=(6535)
 fi
 
 pushd ..
-    for seed in "${SEEDS[@]}"
+    for dataset in "${DATASETS[@]}"
+    do
+        for seed in "${SEEDS[@]}"
         do
-        for dataset in "${DATASETS[@]}"
-        do
+            python run.py \
+                --model.name softki \
+                --model.num_inducing 512 \
+                --model.device $DEVICE \
+                --model.use_qr \
+                --model.use_ard \
+                --model.use_scale \
+                --model.T 1 \
+                --data_dir $DATA_DIR \
+                --dataset.name $dataset \
+                --training.seed $seed \
+                --training.epochs $EPOCHS \
+                --training.learning_rate 0.01 \
+                --dataset.train_frac 0.9 \
+                --dataset.val_frac 0 \
+                --wandb.project $PROJECT \
+                --wandb.group $GROUP \
+                --wandb.watch
+
             python run.py \
                 --model.name svgp \
                 --model.num_inducing 1024 \
                 --model.device $DEVICE \
+                --model.learn_noise \
+                --model.use_ard \
                 --model.use_scale \
                 --data_dir $DATA_DIR \
                 --dataset.name $dataset \
@@ -34,7 +53,6 @@ pushd ..
                 --training.learning_rate 0.01 \
                 --dataset.train_frac 0.9 \
                 --dataset.val_frac 0 \
-                --dataset.num_workers $NUM_WORKERS \
                 --wandb.project $PROJECT \
                 --wandb.group $GROUP \
                 --wandb.watch
@@ -43,6 +61,8 @@ pushd ..
                 --model.name sgpr \
                 --model.num_inducing 512 \
                 --model.device $DEVICE \
+                --model.learn_noise \
+                --model.use_ard \
                 --model.use_scale \
                 --data_dir $DATA_DIR \
                 --dataset.name $dataset \
@@ -51,7 +71,6 @@ pushd ..
                 --training.learning_rate .1 \
                 --dataset.train_frac 0.9 \
                 --dataset.val_frac 0 \
-                --dataset.num_workers $NUM_WORKERS \
                 --wandb.project $PROJECT \
                 --wandb.group $GROUP \
                 --wandb.watch
